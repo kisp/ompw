da1ee7117b035cc939bdca922e90ccb8ac05b827
Kilian Sprotte <kilian.sprotte@gmail.com>
2010-10-07 09:16:52 +0200

    COPYING

--- ompw.lisp
+++ ompw.lisp
@@ -1,4 +1,22 @@
-;;; load file used by OM
+;;; -*- Mode:Lisp; Syntax:ANSI-Common-Lisp; -*-
+
+;;; This file is part of ompw.
+
+;;; Copyright (c) 2007 - 2010, Kilian Sprotte. All rights reserved.
+
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 (flet ((load-compile (path)
 	 (if (member :om-deliver *features*)
 	     (om::compile&load (make-pathname :type nil :defaults path))
